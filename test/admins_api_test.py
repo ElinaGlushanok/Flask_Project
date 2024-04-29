8c95695d041b7a0962cab436dd82e642b7715169
SergeEvdokimov <SergeyEwdokimow@ya.ru>
2024-04-29 15:46:03 +0300

    presentation

--- test/admins_api_test.py
+++ test/admins_api_test.py
@@ -1,14 +1,14 @@
 from requests import get, post
 
-print(get('http://localhost:5000/api/v2/admins').json())
-print(get('http://localhost:5000/api/v2/admins/2').json())
-print(get('http://localhost:5000/api/v2/admins/10').json())
-print(get('http://localhost:5000/api/v2/admins/q').json())
+print(get('https://serg.space/api/v2/admins').json())
+print(get('https://serg.space/api/v2/admins/2').json())
+print(get('https://serg.space/api/v2/admins/10').json())
+print(get('https://serg.space/api/v2/admins/q').json())
 
-print(post('http://localhost:5000/api/v2/admins',
+print(post('https://serg.space/api/v2/admins',
            json={'name': 'Елена', 'surname': 'Летучая', 'keyword': 'qwerty', 'password': '663311'}).json())
-print(post('http://localhost:5000/api/v2/admins',
+print(post('https://serg.space/api/v2/admins',
            json={'name': 'Винтик', 'surname': 'Шпунтик', 'keyword': 'werty', 'password': '663311'}).json())
-print(post('http://localhost:5000/api/v2/admins',
+print(post('https://serg.space/api/v2/admins',
            json={'name': 'Капа', 'surname': 'Баба', 'keyword': 'qwerty', 'password': '663311'}).json())
-print(get('http://localhost:5000/api/v2/admins').json())
+print(get('https://serg.space/api/v2/admins').json())
--- test/orders_api_test.py
+++ test/orders_api_test.py
@@ -1,15 +1,15 @@
 from requests import get, post, delete
 
-print(get('http://localhost:5000/api/v2/orders').json())
-print(get('http://localhost:5000/api/v2/orders/4').json())
-print(get('http://localhost:5000/api/v2/orders/10').json())
-print(get('http://localhost:5000/api/v2/orders/q').json())
-print(post('http://localhost:5000/api/v2/orders', json={'person': 1, 'pause': 3, 'meal': 'макароны-3, кола-8'}).json())
-print(post('http://localhost:5000/api/v2/orders', json={'person': 2, 'pause': 3, 'meal': 'макароны-3'}).json())
-print(post('http://localhost:5000/api/v2/orders',
+print(get('https://serg.space/api/v2/orders').json())
+print(get('https://serg.space/api/v2/orders/4').json())
+print(get('https://serg.space/api/v2/orders/10').json())
+print(get('https://serg.space/api/v2/orders/q').json())
+print(post('https://serg.space/api/v2/orders', json={'person': 1, 'pause': 3, 'meal': 'макароны-3, кола-8'}).json())
+print(post('https://serg.space/api/v2/orders', json={'person': 2, 'pause': 3, 'meal': 'макароны-3'}).json())
+print(post('https://serg.space/api/v2/orders',
            json={'person': 1, 'pause': 3, 'meal': 'макароны-3, котлета-3'}).json())
-print(post('http://localhost:5000/api/v2/orders',
+print(post('https://serg.space/api/v2/orders',
            json={'person': 1, 'pause': 3, 'meal': 'макароны-3, котлета-3', 'status': True}).json())
-print(get('http://localhost:5000/api/v2/orders').json())
-print(delete('http://localhost:5000/api/v2/orders/6').json())
-print(get('http://localhost:5000/api/v2/orders').json())
+print(get('https://serg.space/api/v2/orders').json())
+print(delete('https://serg.space/api/v2/orders/6').json())
+print(get('https://serg.space/api/v2/orders').json())
